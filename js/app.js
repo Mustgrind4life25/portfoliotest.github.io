6f2d8c1e1bd770f31df1a5ce735185abac409300
Mustgrind4life25 <47543212+Mustgrind4life25@users.noreply.github.com>
2022-08-06 22:01:07 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// get contact inputs
+let form = document.getElementById('myform')
+
+let name = document.getElementById('name')
+
+let email = document.getElementById('email')
+
+let btn = document.getElementById('form-btn')
+
+// form.addEventListener('submit', formValidation)	
+
+
+
